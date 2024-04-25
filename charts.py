a57b4774ea0534b31aa2c14b103e6a341ccf374d
YuraYarmola <uarmola1@gmail.com>
2024-04-25 18:32:42 +0300

    make first implementation of program

---
+++ charts.py
@@ -0,0 +1,105 @@
+import flet as ft
+
+
+def get_usage_chart():
+    usage_chart = ft.LineChart(
+        data_series=[
+            ft.LineChartData(
+                data_points=[
+                    # ft.LineChartDataPoint(0, 0),
+                ],
+                stroke_width=1,
+                color=ft.colors.CYAN,
+                curved=True,
+                stroke_cap_round=True,
+            )
+        ],
+        border=ft.border.all(3, ft.colors.with_opacity(0.2, ft.colors.ON_SURFACE)),
+        horizontal_grid_lines=ft.ChartGridLines(
+            interval=20, color=ft.colors.with_opacity(0.2, ft.colors.ON_SURFACE), width=1
+        ),
+        vertical_grid_lines=ft.ChartGridLines(
+            interval=10, color=ft.colors.with_opacity(0.2, ft.colors.ON_SURFACE), width=1
+        ),
+        left_axis=ft.ChartAxis(
+            labels=[
+                ft.ChartAxisLabel(
+                    value=0,
+                    label=ft.Text("0%", size=14, weight=ft.FontWeight.BOLD),
+                ),
+                ft.ChartAxisLabel(
+                    value=20,
+                    label=ft.Text("20%", size=14, weight=ft.FontWeight.BOLD),
+                ),
+                ft.ChartAxisLabel(
+                    value=40,
+                    label=ft.Text("40%", size=14, weight=ft.FontWeight.BOLD),
+                ),
+                ft.ChartAxisLabel(
+                    value=60,
+                    label=ft.Text("60%", size=14, weight=ft.FontWeight.BOLD),
+                ),
+                ft.ChartAxisLabel(
+                    value=80,
+                    label=ft.Text("80%", size=14, weight=ft.FontWeight.BOLD),
+                ),
+                ft.ChartAxisLabel(
+                    value=100,
+                    label=ft.Text("100%", size=14, weight=ft.FontWeight.BOLD),
+                ),
+
+            ],
+            labels_size=40,
+        ),
+        bottom_axis=ft.ChartAxis(
+            labels=[
+                ft.ChartAxisLabel(
+                    value=0,
+                    label=ft.Container(
+                        ft.Text(
+                            "0",
+                            size=16,
+                            weight=ft.FontWeight.BOLD,
+                            color=ft.colors.with_opacity(0.5, ft.colors.ON_SURFACE),
+                        ),
+                        margin=ft.margin.only(top=10),
+                    ),
+                ),
+                ft.ChartAxisLabel(
+                    value=30,
+                    label=ft.Container(
+                        ft.Text(
+                            "30",
+                            size=16,
+                            weight=ft.FontWeight.BOLD,
+                            color=ft.colors.with_opacity(0.5, ft.colors.ON_SURFACE),
+                        ),
+                        margin=ft.margin.only(top=10),
+                    ),
+                ),
+                ft.ChartAxisLabel(
+                    value=60,
+                    label=ft.Container(
+                        ft.Text(
+                            "60",
+                            size=16,
+                            weight=ft.FontWeight.BOLD,
+                            color=ft.colors.with_opacity(0.5, ft.colors.ON_SURFACE),
+                        ),
+                        margin=ft.margin.only(top=10),
+                    ),
+                ),
+            ],
+            labels_size=32,
+        ),
+        tooltip_bgcolor=ft.colors.with_opacity(0.8, ft.colors.BLUE_GREY),
+        min_y=0,
+        max_y=100,
+        min_x=0,
+        max_x=60,
+        animate=2000,
+        expand=False,
+        width=700,
+        height=200,
+    )
+    return usage_chart
--- functions.py
+++ functions.py
@@ -1,5 +1,78 @@
+import clr
 import psutil  # Для отримання інформації про систему
-import time    # Для затримки між оновленнями даних
+import time  # Для затримки між оновленнями даних
+import GPUtil
+import wmi
+
+openhardwaremonitor_hwtypes = ['Mainboard', 'SuperIO', 'CPU', 'RAM', 'GpuNvidia', 'GpuAti', 'TBalancer',
+                               'Heatmaster', 'HDD']
+cputhermometer_hwtypes = ['Mainboard', 'SuperIO', 'CPU', 'GpuNvidia', 'GpuAti', 'TBalancer', 'Heatmaster', 'HDD']
+
+openhardwaremonitor_sensortypes = ['Voltage', 'Clock', 'Temperature', 'Load', 'Fan', 'Flow', 'Control', 'Level',
+                                   'Factor', 'Power', 'Data', 'SmallData']
+cputhermometer_sensortypes = ['Voltage', 'Clock', 'Temperature', 'Load', 'Fan', 'Flow', 'Control', 'Level']
+
+
+class Sensor:
+    def __init__(self):
+        self.sensors = {
+            "Temperature": []
+        }
+        self.handle = self.__initialize_openhardwaremonitor()
+        self.fetch_sensors(self.handle)
+
+        # for key in self.sensors.keys():
+        #     print(f"\n{key}:")
+        #     for sensor in self.sensors[key]:
+        #
+        #         print(sensor.Hardware.Name, sensor.Hardware.HardwareType, sensor.Value)
+
+    def __initialize_openhardwaremonitor(self):
+        file = r'D:\LPNU\SEMESTR 6\SPZ\KURSOVA\SystemMonitor\SPS-CP-Yurii-Yarmola-41\OpenHardwareMonitorLib.dll'
+        clr.AddReference(file)
+
+        from OpenHardwareMonitor import Hardware
+
+        handle = Hardware.Computer()
+        handle.MainboardEnabled = True
+        handle.CPUEnabled = True
+        handle.RAMEnabled = True
+        handle.GPUEnabled = True
+        handle.HDDEnabled = True
+        handle.Open()
+        return handle
+
+    def fetch_sensors(self, handle):
+        for i in handle.Hardware:
+            i.Update()
+            for sensor in i.Sensors:
+
+                if str(sensor.SensorType) in self.sensors.keys():
+                    self.sensors[str(sensor.SensorType)].append(sensor)
+                else:
+                    self.sensors[str(sensor.SensorType)] = [sensor]
+
+            for j in i.SubHardware:
+                j.Update()
+                for subsensor in j.Sensors:
+                    if str(subsensor.SensorType) in self.sensors.keys():
+                        self.sensors[str(subsensor.SensorType)].append(subsensor)
+                    else:
+                        self.sensors[str(subsensor.SensorType)] = [subsensor]
+
+    def parse_sensor(self, sensor):
+        if sensor.Value is not None:
+            if type(sensor).__module__ == 'OpenHardwareMonitor.Hardware':
+                sensortypes = openhardwaremonitor_sensortypes
+                hardwaretypes = openhardwaremonitor_hwtypes
+                hardwaretypes = openhardwaremonitor_hwtypes
+            else:
+                return
+
+            if 'Temperature' in str(sensor.SensorType):
+                print(u"%s %s Temperature Sensor #%i %s - %s\u00B0C" % (
+                    hardwaretypes[sensor.Hardware.HardwareType], sensor.Hardware.Name, sensor.Index, sensor.Name,
+                    sensor.Value))
 
 
 class SystemMonitor:
@@ -19,11 +92,12 @@ class SystemMonitor:
         cpu_usage = psutil.cpu_percent()
         memory_usage = psutil.virtual_memory().percent
         disk_usage = psutil.disk_usage('/').percent
-
+        fans = psutil.sensors_fans()
         print("\n===== System Monitoring =====")
         print(f"CPU Usage: {cpu_usage}%")
         print(f"Memory Usage: {memory_usage}%")
         print(f"Disk Usage: {disk_usage}%")
+        print(f"Fans Speed {fans}")
 
     def set_thresholds(self):
         pass
@@ -44,22 +118,68 @@ class MemoryUsageMonitor:
     def __init__(self):
         pass
 
-    def get_memory_usage(self):
-        pass
+    @staticmethod
+    def get_available_memory():
+        """
+        Get the available physical memory in bytes.
+        """
 
-    def notify_if_threshold_exceeded(self):
-        pass
+        return psutil.virtual_memory().available
+
+    @staticmethod
+    def get_total_memory():
+        """
+        Get the available physical memory in bytes.
+        """
+        return psutil.virtual_memory().total
+
+    @staticmethod
+    def get_used_memory():
+        """
+        Get the used physical memory in bytes.
+        """
+        return psutil.virtual_memory().used
+
+    @staticmethod
+    def get_memory_usage_percentage():
+        """
+        Get the percentage of used physical memory.
+        """
+        return psutil.virtual_memory().percent
 
 
 class DiskSpaceMonitor:
     def __init__(self):
         pass
 
-    def get_disk_space(self):
-        pass
+    def get_all_disks(self):
+        """
+        Get a list of all disk partitions.
+        """
+        return psutil.disk_partitions(all=True)
 
-    def notify_if_threshold_exceeded(self):
-        pass
+    def get_disk_usage(self, disk):
+        """
+        Get disk usage information for a specific disk.
+        """
+        return psutil.disk_usage(disk)
+
+    def get_all_disk_usage(self):
+        """
+        Get disk usage information for all disks.
+        """
+        all_disks = self.get_all_disks()
+        disk_usage_info = {}
+        for disk in all_disks:
+            usage = self.get_disk_usage(disk.mountpoint)
+            disk_usage_info[disk.mountpoint] = {
+                "total": usage.total,
+                "used": usage.used,
+                "free": usage.free,
+                "percent": usage.percent
+
+            }
+        return disk_usage_info
 
 
 class NetworkTrafficMonitor:
@@ -84,12 +204,25 @@ class SystemEventMonitor:
         pass
 
 
-class TemperatureAndFanMonitor:
-    def __init__(self):
-        pass
+class Temperature:
+    def __init__(self, sensors):
+        self.sensors = sensors
 
-    def get_temperature_and_fan_info(self):
-        pass
+    def get_cpu_temperature_sensors(self):
+        sensors_list = []
+        if "Temperature" in str(self.sensors.keys()):
+            for sensor in self.sensors["Temperature"]:
+                if 'cpu' in str(sensor.Hardware.HardwareType).lower():
+                    sensors_list.append(sensor)
+        return sensors_list
+
+    def get_gpu_temperature(self):
+        sensors_list = []
+        if "Temperature" in str(self.sensors.keys()):
+            for sensor in self.sensors["Temperature"]:
+                if 'gpu' in str(sensor.Hardware.HardwareType).lower():
+                    sensors_list.append(sensor)
+        return sensors_list
 
     def notify_if_threshold_exceeded(self):
         pass
